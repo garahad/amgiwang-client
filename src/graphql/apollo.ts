e97ab8456b4ad964dc144ab278705ea0dde0ccce
garahad <bfsudong@gmail.com>
2020-04-16 13:29:23 +0900

    보일러 플레이트로 사용
    Apollo client 부분 설정
    첫 쿼리 날림.

--- src/graphql/apollo.ts
+++ src/graphql/apollo.ts
@@ -1,13 +1,30 @@
 import { ApolloClient } from 'apollo-client';
-import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
 
 const cache = new InMemoryCache();
-const link = new HttpLink({
-  uri: 'http://localhost:4000/',
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql',
 });
 
-const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+const link = ApolloLink.from([errorLink, httpLink]);
+
+const client = new ApolloClient({
   cache,
   link,
+  connectToDevTools: true,
 });
+
+export default client;
