4c02e7eea81ea577710d25fb4c110e283f162b16
garahad <bfsudong@gmail.com>
2020-05-09 13:51:51 +0900

    사이드바 basic 로직 붙임

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { UserOutlined } from '@ant-design/icons';
-import { Layout, Menu, Button } from 'antd';
+import { Layout, Button } from 'antd';
 import { useQuery } from '@apollo/react-hooks';
 
 import { GET_CATEGORIES } from '../graphql/queries';
 
-const { SubMenu } = Menu;
 const { Sider } = Layout;
 
 function Sidebar() {
@@ -14,7 +13,25 @@ function Sidebar() {
     variables: { id: 1 },
   });
 
+  const [categoryAdded, setCategoryAdded] = useState<any>(null);
+  const [domainVisible, setDomainVisible] = useState<boolean[]>([]);
+  const [subdomainInputs, setSubdomainInputs] = useState<boolean[]>([]);
+  const [nowAdding, setNowAdding] = useState<string>('');
+
+  // console.log('dataCategories', dataCategories);
+  // console.log('subdomainInputs', subdomainInputs);
+  // console.log('nowAdding', nowAdding);
+
   let categories;
+
+  useEffect(() => {
+    if (categories && categories.length > 0) {
+      console.log('hello');
+      setSubdomainInputs(Array(categories.length).fill(false));
+      setDomainVisible(Array(categories.length).fill(false));
+    }
+  }, [categories, dataCategories]);
+
   if (dataCategories && dataCategories.getCategories) {
     const categoryDomains = Array.from(
       new Set(dataCategories.getCategories.map((elm) => elm.domain)),
@@ -31,36 +48,121 @@ function Sidebar() {
   }
 
   if (categories && categories.length > 0) {
+    console.log('categories', categories);
     return (
-      <Sider width={200} className="site-layout-background">
-        <Button>+</Button>
-        <Menu
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          style={{ height: '100%', borderRight: 0 }}
+      <Sider width={400} className="site-layout-background">
+        {/* domain 추가 버튼 */}
+        <Button
+          onClick={() => {
+            setCategoryAdded(
+              <span>
+                <UserOutlined />
+                <input type="text" placeholder="입력" />
+                <Button
+                  onClick={() => {
+                    setCategoryAdded(null);
+                  }}
+                >
+                  추가
+                </Button>
+                <Button onClick={() => setCategoryAdded(null)}>취소</Button>
+              </span>,
+            );
+          }}
         >
+          +
+        </Button>
+        <ul>
           {categories.map((elm, key) => (
-            <SubMenu
-              key={key}
-              title={
+            <ul key={key}>
+              {/* domain 리스트 토글버튼 */}
+              <Button
+                onClick={() => {
+                  setDomainVisible(
+                    domainVisible.map((elme, elmKey) => {
+                      if (key === elmKey) {
+                        return !elme;
+                      }
+                      return elme;
+                    }),
+                  );
+                  setSubdomainInputs(
+                    subdomainInputs.map((el, elKey) => {
+                      if (key === elKey && el) {
+                        return false;
+                      }
+                      return el;
+                    }),
+                  );
+                }}
+              >
+                <UserOutlined />
+                {Object.keys(elm)[0]}
+              </Button>
+              {/* subdomain 추가 버튼 */}
+              <Button
+                onClick={() => {
+                  setSubdomainInputs(
+                    subdomainInputs.map((elme, elmKey) => key === elmKey),
+                  );
+                  setDomainVisible(
+                    domainVisible.map((elme, elmKey) => {
+                      if (key === elmKey) {
+                        return true;
+                      }
+                      return elme;
+                    }),
+                  );
+                  // setNowAdding(key);
+                }}
+              >
+                +
+              </Button>
+              {domainVisible[key]
+                ? Object.values<any>(elm)[0]!.map((ele) => (
+                    // ts 이해 부족
+                    <li key={ele} style={{ marginLeft: '30px' }}>
+                      <Link to={`/solve/${Object.keys(elm)[0]}/${ele}/1`}>
+                        {ele}
+                      </Link>
+                      <Button onClick={() => console.log('문제추가')}>+</Button>
+                    </li>
+                  ))
+                : null}
+              {domainVisible[key] && subdomainInputs[key] ? (
                 <span>
-                  <UserOutlined />
-                  {Object.keys(elm)[0]}
+                  <input
+                    type="text"
+                    placeholder="입력"
+                    style={{ marginLeft: '15px' }}
+                  />
+                  <Button
+                    onClick={() => {
+                      console.log('저장');
+                    }}
+                  >
+                    저장
+                  </Button>
+                  <Button
+                    onClick={() => {
+                      setSubdomainInputs(
+                        subdomainInputs.map((elme, elmKey) => {
+                          if (key === elmKey) {
+                            return false;
+                          }
+                          return elme;
+                        }),
+                      );
+                    }}
+                  >
+                    취소
+                  </Button>
                 </span>
-              }
-            >
-              {Object.values<any>(elm)[0]!.map((ele) => (
-                // ts 이해 부족
-                <Menu.Item key={ele}>
-                  <Link to={`/solve/${Object.keys(elm)[0]}/${ele}/1`}>
-                    {ele}
-                  </Link>
-                </Menu.Item>
-              ))}
-            </SubMenu>
+              ) : null}
+            </ul>
           ))}
-        </Menu>
+          {categoryAdded}
+        </ul>
       </Sider>
     );
   }
