810179c55bd5d13a8caceb885b55ed5a6da3c507
garahad <bfsudong@gmail.com>
2020-05-25 13:45:32 +0900

    클릭시 해당 문제 번호로 넘어가는 기능

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,8 +21,8 @@ function Header() {
         defaultSelectedKeys={[]}
         style={{ lineHeight: '64px' }}
       >
-        <Menu.Item key="1">문제 등록</Menu.Item>
-        <Menu.Item key="2">문제 풀기</Menu.Item>
+        {/* <Menu.Item key="1">문제 등록</Menu.Item> */}
+        {/* <Menu.Item key="2">문제 풀기</Menu.Item> */}
         <Menu.Item key="3">문제 검색</Menu.Item>
       </Menu>
     </Layout.Header>
